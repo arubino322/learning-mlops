ece89a19ff3dbdfcdf7ce36337d6387fb00845ff
Andrew Rubino <arubino322@gmail.com>
2024-02-25 22:16:32 -0500

    todo

--- cc-fraud/app.py
+++ cc-fraud/app.py
@@ -7,3 +7,9 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+
+# TODO:
+# what do i want this file to do?
+# read input from a file (csv)
+# read model saved in model directory (or for first past, saved to GCS)
+# print output of the predictions
